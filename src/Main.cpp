41a31ff04a406c0f16cfa3b6667b1381c9ae43bc
Pawel Mielanczyk <cylleondev@gmail.com>
2023-09-10 00:51:36 +0200

    Added Box2d dependenices to project

---
+++ src/Main.cpp
@@ -0,0 +1,19 @@
+﻿// RaylibCppSetup.cpp : Defines the entry point for the application.
+//
+
+#include <raylib-cpp.hpp>
+
+
+int main()
+{
+	raylib::Window window(800, 450, "Raylib CPP");
+	SetTargetFPS(60);
+	while (!window.ShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawText("Bangla", 160, 200, 20, LIGHTGRAY);
+		EndDrawing();
+	}
+	return 0;
+}
